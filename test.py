a9b14c14f644a4fd306eb26739e083c0a99ce50d
teja <tejayanagala205101@gmail.com>
2022-09-10 21:24:46 +0530

    added test.py file

--- test.py
+++ test.py
@@ -23,8 +23,6 @@ def arithmethic_operator(a, b):
         print(d)
     else:
         print('pls enter valid option')
-
-
 arithmethic_operator([[1, 2], [2, 1]], [[2, 1], [3, 5]])
 
 # a = [[1, 2], [2, 1]]
