67f0ed0aa768f0571005f3697ede403f3704b092
teja <tejayanagala205101@gmail.com>
2022-11-17 11:46:42 +0530

    code changes

---
+++ testing.py
@@ -0,0 +1,27 @@
+# # n = int(input().strip())
+# # if n % 2 == 0 and 2 < n < 6:
+# #     print("Not Weird")
+# # elif n % 2 == 0 and 6 < n < 21:
+# #     print("Weird")
+# # elif n % 2 == 0 and n > 20:
+# #     print("Not Weird")
+# # else:
+# #     print('Weird')
+#
+# year = int(input('enter any'))
+# if year % 4 == 0:
+#     print('true')
+# else:
+#     print('false')
+#
+# n = int(input())
+# a = [0]
+# # for i in range(1, n + 1):
+#     a.append(str(a[0]) + str(i))
+# print(a[0])
+
+# print(max(5,13+20,25))
+
+
+n = [1, 2, 5]
+t = 10
